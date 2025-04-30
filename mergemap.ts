d3078772217383de1d198af84a05efe917377c30
Venkst <58278206+venkatraopcareer@users.noreply.github.com>
2025-04-30 12:30:47 +0530

    Update mergemap.ts

--- mergemap.ts
+++ mergemap.ts
@@ -1,6 +1,12 @@
 Merge Map 
 https://www.tektutorialshub.com/angular/using-mergemap-in-angular/
+The MergeMap create a one inner observable for each value of outer observable
+`this.yourFirstService(data).pipe(
+  map(res => // do what ever you want to do which first reques's response),
+  mergeMap(() => this.yourSecondService())).subscribe( finalResponse => {
 
+  });`
+You need to call first service and then map it's response so you can do what ever you do with first service's response and then mergeMap second service. So this way you can resolve you issue.
 of("hound", "mastiff", "retriever")        //outer observable
   .pipe(
     mergeMap(breed => {
